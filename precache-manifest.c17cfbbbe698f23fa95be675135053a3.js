47f58291b9f89a307a511b072d3939b98a8419b8
Nicolás Moreno <niconam2009@hotmail.com>
2020-02-18 08:50:54 -0500

    Updates

---
+++ precache-manifest.c17cfbbbe698f23fa95be675135053a3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4a125b37f2b53211c26be654ea0827",
+    "url": "/pokemon_react/index.html"
+  },
+  {
+    "revision": "4fe11a32eb4f821b177f",
+    "url": "/pokemon_react/static/css/main.2012355c.chunk.css"
+  },
+  {
+    "revision": "c51657aeaa711ed0e33c",
+    "url": "/pokemon_react/static/js/2.3e6c5eb4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pokemon_react/static/js/2.3e6c5eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe11a32eb4f821b177f",
+    "url": "/pokemon_react/static/js/main.0894b6e9.chunk.js"
+  },
+  {
+    "revision": "f20ed17895aa4484fe1e",
+    "url": "/pokemon_react/static/js/runtime-main.b267b22c.js"
+  },
+  {
+    "revision": "9ba4d684e64c19af7c99148ac36af46b",
+    "url": "/pokemon_react/static/media/001.9ba4d684.png"
+  },
+  {
+    "revision": "9be8f2a81b006e2f8897f1889107d0f7",
+    "url": "/pokemon_react/static/media/004.9be8f2a8.png"
+  },
+  {
+    "revision": "7b9ddf692c6df64d86bbe4e96ef46a83",
+    "url": "/pokemon_react/static/media/007.7b9ddf69.png"
+  },
+  {
+    "revision": "799de73ed9a4d32ad6c08e7dc8fcfc50",
+    "url": "/pokemon_react/static/media/010.799de73e.png"
+  },
+  {
+    "revision": "686097819f90747e6b43dfacc5746d36",
+    "url": "/pokemon_react/static/media/Pokemon-logo.68609781.png"
+  },
+  {
+    "revision": "d7a66fc6f20e8776d7c607475ca7b712",
+    "url": "/pokemon_react/static/media/battle.d7a66fc6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokemon_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
